c01db4f38fddfd385ac97b54847b355f9fb45136
lqjtj <lqj_tj@sina.com>
2017-05-19 17:58:34 +0800

    add role.vue

---
+++ src/components/admin/Role.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+ 
+  </div>
+</template>
+
+<script>
+ export default {
+    data() {
+      return {
+       
+      }
+    },
+    methods: {
+      onSubmit() {
+        let _this=this;
+
+       var params = new URLSearchParams();
+            params.append('client', this.name);
+            params.append('pwd', this.pwd);
+
+         this.$http.post('/KindergartenWeb/action/login.do',params).then(function (res) {
+            var obj = {}
+            for (let i = 0; i < res.data.rows.length; i++) {
+                obj.msg = res.data.total
+                obj.client = res.data.rows[i].client
+                obj.name = res.data.rows[i].name
+            }
+            alert(JSON.stringify(obj));
+                    window.localStorage.setItem("name",obj.name);
+            //window.localStorage.setItem("pwd",this.pwd);
+            _this.$router.push("/main");
+         })
+ 
+
+      },
+     onReset() {
+        this.name="";
+        this.pwd="";
+      }      
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
