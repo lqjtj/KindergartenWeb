94006f70fe7dd993635eec2af2f15269a691727b
lqjtj <lqj_tj@sina.com>
2017-05-27 10:52:30 +0800

    170527

--- src/components/admin/RolePrivilege.vue
+++ src/components/admin/RolePrivilege.vue
@@ -1,154 +1,328 @@
 <template>
   <div>
-      <el-table
-        :data="roleList"
-        highlight-current-row
-        @current-change="handleCurrentChange"
-        style="width: 20%">
+     <div style="float:left;">
+          <el-table
+            :data="roleList"
+            highlight-current-row
+            @current-change="handleCurrentChange"
+            style="width: 80">
+            <el-table-column
+              property="id"
+              label="#"
+              style="width:10">
+            </el-table-column>    
+            <el-table-column
+              property="title"
+              label="角色"
+              style="width:50">
+            </el-table-column>
+          </el-table>
+     </div>
+    <div style="float:left;margin-left:20px;">
+          <el-table
+          :data="privilegeList"
+          border
+          tooltip-effect="dark">
+
+          <el-table-column
+            prop="resourceId"
+            label="#"
+            width="80">
+          </el-table-column>
+          <el-table-column
+            prop="resourceTitle"
+            label="title"
+            width="150">
+          </el-table-column>
+          <el-table-column 
+            :render-header="renderHeader" 
+            width="80">
+          <template scope="scope">
+              <el-checkbox v-model="scope.row.cs"></el-checkbox>
+            </template>      
+          </el-table-column>
+          
+          <el-table-column
+            :render-header="renderHeader" 
+            width="80">
+            <template scope="scope">
+              <el-checkbox v-model="scope.row.ci"></el-checkbox>
+            </template>      
+          </el-table-column>
+          <el-table-column
+            :render-header="renderHeader" 
+            width="80">
+            <template scope="scope">
+              <el-checkbox v-model="scope.row.cu"></el-checkbox>
+            </template>      
+          </el-table-column>
+          <el-table-column
+            :render-header="renderHeader" 
+            width="80">
+            <template scope="scope">
+              <el-checkbox v-model="scope.row.cd"></el-checkbox>
+            </template>
+          </el-table-column>
         <el-table-column
-          property="id"
-          label="#"
-          style="width: 20%">
-        </el-table-column>    
-        <el-table-column
-          property="title"
-          label="角色"
-          style="width: 80%">
-        </el-table-column>
-      </el-table>
-
-
-     <el-table
-    :data="tableData3"
-    border
-    tooltip-effect="dark"
-    @selection-change="handleSelectionChange">
-
-    <el-table-column
-      label="日期"
-      width="120">
-      <template scope="scope">{{ scope.row.date }}</template>
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="120">
-    </el-table-column>
-     <el-table-column
-      :render-header="renderHeader" 
-      width="100">
-     <template scope="scope">
-         <el-checkbox v-model="scope.row.cs"></el-checkbox>
-      </template>      
-    </el-table-column>
-     
-    <el-table-column
-      :render-header="renderHeader" 
-      width="100">
-      <template scope="scope">
-         <el-checkbox v-model="scope.row.ci"></el-checkbox>
-      </template>      
-    </el-table-column>
-    <el-table-column
-      :render-header="renderHeader" 
-      width="100">
-      <template scope="scope">
-         <el-checkbox v-model="scope.row.cu"></el-checkbox>
-      </template>      
-    </el-table-column>
-    <el-table-column
-      :render-header="renderHeader" 
-      width="100">
-      <template scope="scope">
-         <el-checkbox v-model="scope.row.cd"></el-checkbox>
-      </template>
-    </el-table-column>
-   <el-table-column
-      :render-header="renderHeader" 
-      width="100">
-       <template scope="scope">
-         <el-checkbox v-model="scope.row.co"></el-checkbox>
-      </template>
-    </el-table-column>                
-  </el-table>     
+            :render-header="renderHeader" 
+            width="80">
+            <template scope="scope">
+              <el-checkbox v-model="scope.row.co"></el-checkbox>
+            </template>
+          </el-table-column>  
+            <el-table-column
+      type="selection"
+      width="55">##
+            </el-table-column>              
+        </el-table>  
+    </div>
+    <div style="clear:both;">
+         <el-button type="primary" @click="onSave">保存</el-button> 
+         <el-checkbox v-model="csall" >select</el-checkbox>
+    </div> 
   </div>
 </template>
 
 <script>
  export default {
+    watch: {
+        csall: function (val, oldVal) {
+          console.log('new: %s, old: %s', val, oldVal)
+          for (let i = 0; i < this.privilegeList.length; i++) {
+                 this.privilegeList[i].cs=val;
+            }
+        },
+      ciall: function (val, oldVal) {
+            for (let i = 0; i < this.privilegeList.length; i++) {
+                 this.privilegeList[i].ci=val;
+            }
+        },
+      cuall: function (val, oldVal) {
+            for (let i = 0; i < this.privilegeList.length; i++) {
+                 this.privilegeList[i].cu=val;
+            }
+        },
+      cdall: function (val, oldVal) {
+             for (let i = 0; i < this.privilegeList.length; i++) {
+                 this.privilegeList[i].cd=val;
+            }
+        },
+      coall: function (val, oldVal) {
+            for (let i = 0; i < this.privilegeList.length; i++) {
+                 this.privilegeList[i].co=val;
+            }
+        }                    
+    },
     data() {
       return {
         roleList:[],
-        tableData3: [{
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
+        csall:false,
+        ciall:false,
+        cuall:false,
+        cdall:false,
+        coall:false,
+        privilegeList: [{
+          roleId:'',
+          resourceId: '2016-05-03',
+          resourceTitle: '王小虎',
           cs:true,
           ci:false,
           cu:false,
           cd:true,
           co:true
         }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
+          roleId:'',
+          resourceId: '2016-05-03',
+          resourceTitle: '王小虎',
           cs:true,
           ci:false,
           cu:false,
           cd:true,
           co:true
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-           cs:true,
-          ci:false,
-          cu:false,
-          cd:true,
-          co:true
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-           cs:true,
-          ci:false,
-          cu:false,
-          cd:true,
-          co:true
-        }, {
-          date: '2016-05-08',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-          cs:true,
-          ci:false,
-          cu:false,
-          cd:true,
-          co:true
-        }],
-        multipleSelection: []
+        }]
       }
     },
     created: function () {
            this.getData();
     }  ,    
     methods: {
+      onSave(){
+          let _this=this
+          var params_json=this.privilegeList
+           //alert(JSON.stringify(params_json));
+            this.$http.post('/KindergartenWeb/action/role/privilege.do',params_json,{withCredentials:true}).then(function (res) {
+          
+          if(JSON.stringify(res.data)=='1'){
+                _this.$message({
+                  showClose: true,
+                  message: '保存成功！',
+                  type: 'success'
+              });
+          }else{
+               _this.$message({
+                  showClose: true,
+                  message: '保存失败！',
+                  type: 'success'
+              });           
+          }
+        }).catch(function (error) {
+            _this.$message.error(error);
+        })     
+      },
+       getPrivilege(roleid){
+          var j=0;
+         var data = [];
+         let _this=this;
+
+       var params_json={
+                         userId:'15122922900',
+                         action:'getPrivilege',
+                         param1:roleid,
+                         param2:'',
+                         param3:'',
+                         param4:'',
+                         param5:''};
+                 this.$http.post('/KindergartenWeb/action/list.do',params_json).then(function (res) {
+           //alert(JSON.stringify(res.data));
+           for (let i = 0; i < res.data.rows.length; i++) {
+                var obj = {}
+                obj.roleId=roleid
+                obj.resourceId = res.data.rows[i].resourceId
+                obj.resourceTitle = res.data.rows[i].resourceTitle
+                if(res.data.rows[i].cs=='1')
+                    obj.cs=true
+                else
+                    obj.cs=false
+
+                if(res.data.rows[i].ci=='1')
+                    obj.ci=true
+                else
+                    obj.ci=false
+
+               if(res.data.rows[i].cu=='1')
+                    obj.cu=true
+                else
+                    obj.cu=false
+
+               if(res.data.rows[i].cd=='1')
+                    obj.cd=true
+                else
+                    obj.cd=false
+
+               if(res.data.rows[i].co=='1')
+                    obj.co=true
+                else
+                    obj.co=false                                                                               
+ 
+                  data[j++] = obj
+            }
+           
+            _this.privilegeList = data
+           
+        }).catch(function (error) {
+               alert(error);
+        }) 
+ },      
      renderHeader(h, { column, $index }){
-         if($index==2)
-           return (<span> <el-checkbox >Select</el-checkbox></span>);
-         else if($index==3)
-             return (<span> <el-checkbox >insert</el-checkbox></span>);
-         else if($index==4)
-             return (<span> <el-checkbox >update</el-checkbox></span>);
-         else if($index==5)
-             return (<span> <el-checkbox >delete</el-checkbox></span>);
-         else if($index==6)
-             return (<span> <el-checkbox >other</el-checkbox></span>);                                       
+       var self = this
+
+     if($index==22){
+             return (<span> <el-checkbox  >other</el-checkbox></span>);                                       
+     }   
+     else if($index==2){
+          return  h('el-checkbox', {
+                      domProps: {
+                        label:'查询',
+                        checked:this.csall,
+                        value: self.value,
+                        id:'idcsall'
+                      },
+                      props: ["csall"],
+                       watch: {
+                       [self.csall]: function (val, oldVal) {
+                        alert("get it");
+                        console.log('new: %s, old: %s', val, oldVal)
+                      }
+                       },
+                      on: {
+                        change: function (e) {
+                          self.csall= e.target.checked
+                        }
+                      }
+                    }
+                )
+         }
+         else if($index==3){
+              return  h('el-checkbox', {
+                            domProps: {
+                              label:'新建',
+                              checked:this.ciall,
+                              value: self.value
+                            },
+                            on: {
+                              change: function (e) {
+                                self.ciall= e.target.checked
+                              }
+                            }
+                          }
+                      )
+         }
+         else if($index==4){
+              return  h('el-checkbox', {
+                              domProps: {
+                                label:'修改',
+                                checked:this.cuall,
+                                value: self.value
+                              },
+                              on: {
+                                change: function (e) {
+                                  self.cuall= e.target.checked
+                                }
+                              }
+                            }
+                        )
+         }
+         else if($index==5){
+              return  h('el-checkbox', {
+                              domProps: {
+                                label:'删除',
+                                checked:this.cdall,
+                                value: self.value
+                              },
+                              on: {
+                                change: function (e) {
+                                  self.cdall= e.target.checked
+                                }
+                              }
+                            }
+                        )
+         }
+         else if($index==6){
+                return  h('el-checkbox', {
+                                domProps: {
+                                  label:'其它',
+                                  checked:this.coall,
+                                  value: self.value
+                                },
+                                on: {
+                                  change: function (e) {
+                                    self.coall= e.target.checked
+                                  }
+                                }
+                              }
+                          )
+         }
+            // return (<span> <el-checkbox v-model="coall" on-change={()=> this.onChange( column, $index)}>other</el-checkbox></span>);                                       
      },
-     handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },      
        handleCurrentChange(val) {
-        alert(val.id);
+        this.csall=false
+        this.ciall=false
+        this.cuall=false
+        this.cdall=false
+        this.coall=false
+        //document.getElementById("idcsall").checked=false;
+
+        this.getPrivilege(val.id);
       } ,     
         getData(){
         var j=0;
